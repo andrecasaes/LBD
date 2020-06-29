febfeae7971a3fdcedeac1e0376c7ecee33d2608
André Biondi Casaes <andre.casaes@usp.br>
2020-06-29 18:41:17 -0300

    Delete teste.php

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php include 'Genericos/conecta.php'; ?>
-<?php
-    $tsql = "SELECT * FROM tb_sala";
-    $dados   = sqlsrv_query($conn, $tsql);
-
-    while($row = sqlsrv_fetch_array($dados, SQLSRV_FETCH_ASSOC)) {
-        echo $row['nroID']."<br>";
-    }
-?>
